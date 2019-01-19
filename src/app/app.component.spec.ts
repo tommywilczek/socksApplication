1a65e0936e37063186dc37b235e5a92f100c0166
Tommy Wilczk <tommywilczek@utexas.edu>
2019-01-19 16:24:10 -0600

    tests in the right place

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from '../app/app.component';
-import { ShowSocksComponent } from '../app/show-socks/show-socks.component';
+import { AppComponent } from './app.component';
+import { ShowSocksComponent } from './show-socks/show-socks.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -33,13 +33,6 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-socks!');
-  });
-
-  it('should create show-socks component', () => {
-    const fixture = TestBed.createComponent(ShowSocksComponent);
-    fixture.detectChanges(); 
-    const compiled = fixture.debugElement.nativeElement;
-    expect(ShowSocksComponent).toBeTruthy();
   });
 
 });
